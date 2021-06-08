e343ba516f95120cae76e76b3361cb0fed7c6e32
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-06-08 10:03:14 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesComponent } from './features/features.component';
+import { WorksComponent } from './works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeaturesComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule,
